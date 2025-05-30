1979ebd9f5c45d71de2e8538cc14a8f5ce06c929
Eduard0w <dudusouza5595@gmail.com>
2025-05-30 16:37:38 -0300

    acrescento da classe no e metodo push na pilha encadeada

--- pilha/PilhaEncadeada.java
+++ pilha/PilhaEncadeada.java
@@ -1,9 +1,50 @@
 package pilha;
 
-public class PilhaEncadeada {
+public class PilhaEncadeada<T> {
+	private No<T> topo; // referencia do topo da pilha, ou seja, seguira o utimo elemento acrescentado.
+	
+	public class No<T> {
+		private T elemento;
+		private No<T> proximo;
+		
+		public No (T elemento) {
+			this.elemento = elemento;
+			this.proximo = null;
+		}
+
+		public T getElemento() {
+			return elemento;
+		}
+
+		public void setElemento(T elemento) {
+			this.elemento = elemento;
+		}
+
+		public No<T> getProximo() {
+			return proximo;
+		}
+
+		public void setProximo(No<T> proximo) {
+			this.proximo = proximo;
+		}
+		
+	}
+	
+	public void push(T elemento) {
+		
+		No<T> novoNo = new No<>(elemento);
+		novoNo.setProximo(topo);
+		topo = novoNo;
+	
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		PilhaEncadeada<Integer> pilha = new PilhaEncadeada<Integer>();
+		
+		pilha.push(2);
+		pilha.push(3);
+		pilha.push(10);
+		
 
 	}
 
