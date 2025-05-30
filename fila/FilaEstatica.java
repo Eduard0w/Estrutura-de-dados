9f1d4aaf4a4516e4cee0b2d5173a99561882eda8
Eduard0w <dudusouza5595@gmail.com>
2025-05-30 19:00:45 -0300

    avanço na filaEstatica

--- fila/FilaEstatica.java
+++ fila/FilaEstatica.java
@@ -1,6 +1,22 @@
 package fila;
 
-public class FilaEstatica{
+public class FilaEstatica<T>{
+	private T elementos[];
+	private int inicio;
+	private int fim;
+	private int tamanho;
+	
+	@SuppressWarnings("unchecked")
+	public FilaEstatica(int tamanho) {
+		this.elementos = (T[]) new Object[tamanho];
+		this.inicio = this.fim = 0;
+		this.tamanho = tamanho;
+	}
+	
+	public void Enqueue(T elemento) {
+		this.elementos[++this.fim] = elemento;
+		this.inicio = elementos;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
