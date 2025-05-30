d27d59999af7d65d047ca8860b401aeea43790d4
Eduard0w <dudusouza5595@gmail.com>
2025-05-30 18:40:21 -0300

    termino da classe PilhaEncadeada e nova package fila

--- pilha/PilhaEncadeada.java
+++ pilha/PilhaEncadeada.java
@@ -30,6 +30,10 @@ public class PilhaEncadeada<T> {
 		
 	}
 	
+	public boolean isVazio() {
+		return this.topo == null;
+	}
+	
 	public void push(T elemento) {
 		
 		No<T> novoNo = new No<>(elemento);
@@ -37,14 +41,47 @@ public class PilhaEncadeada<T> {
 		topo = novoNo;
 	
 	}
+	
+	public void pop() {
+		if(isVazio()) {
+//			return null;
+			return;
+		}
+//		T elemento = topo.getElemento();
+		topo = topo.getProximo(); // move topo para o proximo nó
+//		return elemento;
+	}
+	
+	public T peek() {
+		if(isVazio()) {
+			return null;
+		}
+		
+		return topo.getElemento();
+	}
+	
+	public void print() {
+		No<T> atual = topo;
+		
+		while(atual != null) {
+			System.out.println("| "+ atual.getElemento() + " |");
+			atual = atual.getProximo();
+		}
+	}
 
 	public static void main(String[] args) {
 		PilhaEncadeada<Integer> pilha = new PilhaEncadeada<Integer>();
 		
+		System.out.println("---Pilha completa---\n");
 		pilha.push(2);
 		pilha.push(3);
 		pilha.push(10);
-		
+		pilha.peek();
+		pilha.print();
+		System.out.println("\nRemoção...");
+		pilha.pop();
+		System.out.println("\n---Depois da remoção---\n");
+		pilha.print();
 
 	}
 
