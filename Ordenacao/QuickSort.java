b057b1e369d9e37acefc74cbad2c4d7ea9e4bb46
Eduard0w <dudusouza5595@gmail.com>
2025-07-14 11:23:53 -0300

    finalização do Quick Sort (Lomuto partition)

--- Ordenacao/QuickSort.java
+++ Ordenacao/QuickSort.java
@@ -1,25 +1,54 @@
 package Ordenacao;
 
+import java.util.Scanner;
+
 public class QuickSort {
-	
-	public static void quickSort(int vetor[], int inicio, int fim) {
+	//método de resolusão: Lomuto partition
+	public static int partition(int vetor[], int inicio, int fim) {		
 		int pivo = vetor[fim];
-		if(pivo <= inicio) {
-			return;
+		
+		int i = inicio - 1;
+		
+		for(int j = inicio; j <= fim; j++) {
+			if(vetor[j] < pivo) {
+				i++;
+				swap(vetor, i, j);
+			}
 		}
 		
-		for(int i=0; i<vetor.length; i++) {
-			if(pivo < vetor[i]) {
-				int temp = vetor[inicio];
-				vetor[inicio] = vetor[i];
-				
-			}
+		
+		swap(vetor, i+1, fim);
+		return i+1;
+	}
+	
+	
+	public static void swap(int[] value, int i, int j) {
+		int temp = value[i];
+	    value[i] = value[j];
+	    value[j] = temp;
+	}
+	
+	
+	public static void quickSort(int[] vetor, int inicio, int fim) {
+		if (inicio < fim) {
+			
+			 int pi = partition(vetor, inicio, fim);
+			 
+			 quickSort(vetor, inicio, pi - 1);
+			 quickSort(vetor, pi + 1, fim);
 		}
 	}
 
 	public static void main(String[] args) {
-		int vetor[] = {2, 3, 5, 1, 4};
+		Scanner t = new Scanner(System.in);
+		int vetor[] = {2, 4, 5, 1, 3};
 		
+		quickSort(vetor, 0, vetor.length - 1);
+		
+		System.out.println("Vetor organizado: ");
+		for(int i=0; i< vetor.length; i++) {
+			System.out.print(vetor[i] +", ");
+		}
 	}
 
 }
--- Ordenacao/Sobre_Ordenação.md
+++ Ordenacao/Sobre_Ordenação.md
@@ -10,8 +10,8 @@
 .
 .
 .
-## ...
-.
-.
+## Quick Sort
+. Existem Três maneira de resolver essa ordenação, a Naive Partition, Lomuto Partition e a Hoare's Partition (essa 
+. é a mais rapida das três)
 .
 .
